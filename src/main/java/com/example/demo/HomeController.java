a37572bcf182910fe0a8ae464e8c6fad7e93d6db
kellel14 <kellel14@gmail.com>
2019-07-08 17:25:16 -0400

    first commit

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping
+    public String mainPage(){
+        return "/index1";
+    }
+
+    @RequestMapping
+    public String pageTwo(){
+        return "/index2";
+    }
+
+    @RequestMapping
+    public String pageThree(){
+        return "/index3";
+    }
+
+}
